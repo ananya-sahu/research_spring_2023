05b715518950999ea025ea3e3af59cbcf4f7b3ab
Ananya Sahu <ananya1sahu@gmail.com>
2023-04-18 20:01:15 -0400

    adds similarity scoring

--- extract_data.py
+++ extract_data.py
@@ -6,7 +6,7 @@ import random
 from collections import defaultdict
 from feat import Detector 
 
- 
+
 #filter annotations for only video type data 
 def filter_only_video(dictionary):
     return dict((k, dictionary[k]) for k in dictionary.keys() if dictionary[k]['data_type'] == 'video')
@@ -47,20 +47,9 @@ def extract_features_file(detector, directory, frames):
     # return df_all
     return dfs
 
-
-def get_similarity(df, features):
-    sim = []
-    df = df[features]
-    for i in range(len(df.index)-1):
-        for j in range(i+1,len(df.index)):
-            sim.append(np.dot(df.iloc[i], df.iloc[j])/(np.linalg.norm(df.iloc[i])*np.linalg.norm(df.iloc[j])))
-    
-    return sum(sim) / len(sim)
-
 def extract(video_change_points, annotations,detector, split):
     all_features = {}
-    for file_id in annotations:
-        print(file_id)
+    for file_id in video_change_points:
         if annotations[file_id]['split'] == split and annotations[file_id]['processed'] == True and (len(video_change_points[file_id]) >0):
             features = {}
             directory = annotations[file_id]['processed_dir']
@@ -78,7 +67,7 @@ def non_change_points(start, end, change_points):
         while t <= change_point-5 and t>= change_point+5 :
             t = random.uniform(start+5, end-5)
             # random.randint(start+5, end-5)
-    return t
+    return [t]
 
 def get_non_change_points(annotations, video_change_points):
     non_change_points_dict = {}
@@ -87,7 +76,49 @@ def get_non_change_points(annotations, video_change_points):
         start = annotations[file_id]['start']
         end = annotations[file_id]['end']
         non_change_points_dict[file_id] = non_change_points(start, end, video_change_points[file_id])
-    return non_change_points
+    return non_change_points_dict
+
+def get_similarity(df, features):
+    sim = []
+    df = df[features]
+    for i in range(len(df.index)-1):
+        for j in range(i+1,len(df.index)):
+            sim.append(np.dot(df.iloc[i], df.iloc[j])/(np.linalg.norm(df.iloc[i])*np.linalg.norm(df.iloc[j])))
+    
+    if len(sim) == 0:
+        return None
+    return sum(sim) / len(sim)
+
+def similarity_scores(features_dict, feature, label):
+    df = pd.DataFrame(columns=['file_id','time_stamp', 'sim1',
+                             'sim2','sim3', 'label'])
+    sims = {}
+    for file_id in features_dict:
+        for t in features_dict[file_id]:
+            frames_t = {}
+            sim = []
+            frames = features_dict[file_id][t]
+            for f in frames:
+                if len(f.index) >0:
+                    if feature == 'all':
+                        features = list(set(f.columns) - set(['FaceRectX', 'FaceRectY', 'FaceRectWidth', 'FaceRectHeight','FaceScore', 'label', 'input', 'frame']))
+                    if feature == 'emotion':
+                        # features = ['anger', 'disgust', 'fear', 'happiness', 'sadness', 'surprise','neutral']
+                        features = f.emotions.columns
+                    if feature == 'musc':
+                        features = f.aus.columns
+                    if feature == 'head_pose':
+                        features = f.poses.columns
+                    sim.append(get_similarity(f, features))
+
+            if any(x is None for x in sim) == False:
+                frames_t[t] = sim
+                sims[file_id] = frames_t
+                row = [file_id, t]
+                row.extend(sim)
+                row.append(label)
+                df.loc[len(df)] = row
+    return sims,df
 
 def main():
     #load original annotations
@@ -99,8 +130,8 @@ def main():
     # with open("./video_annotations.pkl", 'wb') as f:
     #         pickle.dump(annotations, f)
     
-    with open('/home/as5957/research_spring_2023/video_annotations.pkl', 'rb') as handle:
-        annotations = pickle.load(handle)
+    # with open('/home/as5957/research_spring_2023/video_annotations.pkl', 'rb') as handle:
+    #     annotations = pickle.load(handle)
     
     #get changepoints a video file and add as a list 
     # change_points = defaultdict(list)
@@ -113,8 +144,8 @@ def main():
     #         pickle.dump(change_points, f)
 
    #{'file_id': [timestamps of change points]}
-    with open("./video_changepoints.pkl", 'rb') as handle:
-        video_change_points = pickle.load(handle)
+    # with open("./video_changepoints.pkl", 'rb') as handle:
+    #     video_change_points = pickle.load(handle)
     
     # frames = get_frames(annotations, 'M01000AJ9', 94.0)
     # directory = annotations['M01000AJ9']['processed_dir']
@@ -129,25 +160,43 @@ def main():
     #     pickle.dump(change_point_features, f)
 
     #get non change points 
-    # non_change_points = get_non_change_points(annotations, video_change_points)
+    # non_change_points_dict = get_non_change_points(annotations, video_change_points)
+    # print(non_change_points_dict)
     # with open("./non_change_points.pkl", 'wb') as f:
-    #     pickle.dump(non_change_points, f)
+    #     pickle.dump(non_change_points_dict, f)
     
     #get non change_point features
-    with open("./non_change_points.pkl", 'rb') as handle:
-        non_change_points = pickle.load(handle)
+    # with open("./non_change_points.pkl", 'rb') as handle:
+    #     non_change_points_dict = pickle.load(handle)
     
-    detector = Detector()
-    non_change_point_features = extract(non_change_points,annotations,detector,'train')
-    with open("./non_change_point_features.pkl", 'wb') as f:
-        pickle.dump(non_change_point_features, f)
-
-
-    
+    # detector = Detector()
+    # non_change_point_features = extract(non_change_points_dict,annotations,detector,'train')
+    # with open("./non_change_point_features.pkl", 'wb') as f:
+    #     pickle.dump(non_change_point_features, f)
     
 
+    with open("./non_change_point_features.pkl", 'rb') as handle:
+        non_change_points_frames = pickle.load(handle)
+
+    with open("./change_point_features.pkl", 'rb') as handle:
+        change_points_frames = pickle.load(handle)
+    
+    sims_dict_cp, sims_df_cp = similarity_scores(change_points_frames,'head_pose', 'cp')
+    sims_dict_ncp, sims_df_ncp = similarity_scores(non_change_points_frames,'head_pose', 'ncp')
+    
+    with open("./non_change_point_sims_pose.pkl", 'wb') as f:
+        pickle.dump(sims_dict_ncp, f)
+
+    with open("./change_point_sims_pose.pkl", 'wb') as f:
+        pickle.dump(sims_dict_cp, f)
+    
+
+    sims_df_cp.to_csv("./change_point_sims_pose.csv")
+    sims_df_ncp.to_csv("./nonchange_point_sims_pose.csv")
 
 
+
+    
 
 
 if __name__ == "__main__":
