8599cd27a36daa505d62c503d783c7ee932011bf
Ananya Sahu <ananya1sahu@gmail.com>
2023-04-27 20:44:37 -0400

    adds fitlering for dyadic videos

---
+++ filter.py
@@ -0,0 +1,22 @@
+import pickle 
+
+with open('/home/as5957/research_spring_2023/change_point_features.pkl', 'rb') as f:
+    all_features = pickle.load(f)
+
+print(len(all_features))
+
+filtered_time = {}
+filtered = {}
+for id in all_features:
+    change_points = all_features[id]
+    for time in change_points:
+        dfs = change_points[time]
+        if all(len(d) == 2 for d in dfs):
+            filtered_time[time] = dfs
+    if len(filtered_time) >0:
+        filtered[id] = filtered_time
+
+print(len(filtered))
+
+with open("./change_points_two.pkl", 'wb') as f:
+    pickle.dump(filtered, f)
